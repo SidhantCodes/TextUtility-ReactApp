1684ed6bdfd751be0a6db3e6d9de4023688cc297
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-12-30 18:08:33 +0530

    Semi Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+import { useState } from 'react';
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  
+  const [mode, setMode] = useState('light');
+  const [txt, setTxt] = useState("Toggle Dark Mode");
+  const [txtMode, setTxtMode] = useState('dark');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      setTxt("Toggle Light Mode");
+      setTxtMode('light');
+      document.body.style.backgroundColor = 'rgb(28 28 28)';
+      document.body.style.color = 'white';
+      showAlert("Dark mode enabled", "success");
+    } else {
+      setMode('light');
+      setTxt("Toggle Dark Mode");
+      setTxtMode('dark');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode enabled", "success");
+    }
+  }
+
   return (
     <>
-    
-    <Navbar title={"Text Refactor"}/>
-    <div className="container my-3">
-      <TextForm title={"Enter the text here: "}/>
-    </div>
+      <BrowserRouter>
+        <Navbar title={"Text Refactor"} Mode={mode} btnTxt={txt} toggleMode={toggleMode} txtMode={txtMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About Mode={mode}/>} />
+            <Route exact path="/" element={<TextForm title={"Enter the text here: "} Mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+  // const wordCheck = (word) => {
+  //   if(word.split(" ").length===0){
+  //     return false
+  //   }else{
+  //     return true
+  //   }
+  // }
   const UpperCase = () => {
     console.log("Upper case was clicked.");
     setText(text.toUpperCase());
+    props.showAlert("Converted to UPPER case", "success");
   }
   const LowerCase = () => {
     console.log("Lower case was clicked.");
     setText(text.toLowerCase());
+    props.showAlert("Converted to lower case", "success");
   }
   const Clear = () => {
     setText("");
+    props.showAlert("Cleared the text area", "success");
+  }
+  const CopyClip = () => {
+    navigator.clipboard.writeText(text)
+    props.showAlert("Copied to clipboard", "success");
   }
   const HandleOnChange = (event) => {
     console.log("on change");
@@ -23,20 +37,21 @@ export default function TextForm(props) {
       <form>
         <h3>{props.title}</h3>
         <div className="mb-3">
-          <textarea className="form-control" id="myBox" value={text} onChange={HandleOnChange} rows={8}></textarea>
+          <textarea className="form-control" id="myBox" value={text} onChange={HandleOnChange} rows={8} style={{backgroundColor: props.Mode==='dark'?'black':'white', color: props.Mode==='dark'?'white':'black' }}></textarea>
         </div>
-        <button type='button' className="btn btn-outline-danger" onClick={UpperCase}>Convert to upper case</button>
-        <button type='button' className="btn btn-outline-danger mx-2" onClick={LowerCase}>Convert to Lower case</button>
-        <button type='button' className="btn btn-outline-danger mx-2" onClick={Clear}>Clear Text</button>
+        <button disabled={text.length===0} type='button' className="btn btn-outline-danger mx-2 my-1" onClick={UpperCase}>Convert to upper case</button>
+        <button disabled={text.length===0} type='button' className="btn btn-outline-danger mx-2 my-1" onClick={LowerCase}>Convert to Lower case</button>
+        <button disabled={text.length===0} type='button' className="btn btn-outline-danger mx-2 my-1" onClick={Clear}>Clear Text</button>
+        <button disabled={text.length===0} type='button' className="btn btn-outline-danger mx-2 my-1" onClick={CopyClip}>Copy to Clipboard</button>
       </form>
     </div>
     <div className="container my-3">
       <h4>Your text summary: </h4>
-      <p>word count: {text.split(" ").length - 1}</p>
+      <p>word count: {text.split(" ").filter((element)=>{return element.length!==0}).length}</p>
       <p>Character count: {text.length}</p>
       <p>Time to read: {0.008 * (text.split(" ").length-1)}</p>
       <h4>Text preview: </h4>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter text to preview."}</p>
     </div>
     </>
   )
